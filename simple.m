0876220f3e4a2359d3ff371afc6eeb4012c696f6
Jan Kowalski <srgx@users.noreply.github.com>
2023-08-31 22:36:09 +0200

    Substances, atoms and molecules

--- simple.m
+++ simple.m
@@ -10,6 +10,8 @@ m = 120; d = 10.5; v = m / d
 # Aluminum
 v = 12; m = 32.4; d = m / v
 
+########################################
+
 # Lead
 v = 2; d = 11.3; m = v * d
 
@@ -24,5 +26,78 @@ d = 9; v = 1000; m = d * v
 
 # Zinc
 d = 7.1; v = 10; m = v * d
+
+########################################
+
+# Aluminum data
+aluminum = [13 3 3 27]
+
+# Neutrons and electrons
+atom = [119 - 50, 50]
+
+# Electrons in ion
+electrons = 26 - 2
+
+# Protons, electrons and neutrons
+proEleNeu = [8 8 16 - 8]
+
+# Period and group
+pos = [2 16]
+
+# Element with 35 protons
+pos = [4 17]
+
+# Symbols of elements satisfying conditions
+els = {"As" "Si" "Br"}
+
+# Carbon nucleons
+nucleons = 6 + 7
+
+# Sulfur data
+sulfur = [16 16 33 - 16 6 3]
+
+# Sodium data
+sodium = {"Na" "Sodium" 3 11 11 1}
+
+########################################
+
+# Sulfur protons, electrons and neutrons
+sulfur = [16 16 33 - 16]
+
+# Zinc 70 protons and neutrons
+zinc = [30 70 - 30]
+
+########################################
+
+# Sulfur (S)
+sulfurMass = 64 - 32
+
+# Hydrogen atoms in ammonia
+nHydrogen = 17 - 14
+
+# Gallium - Ga2O3
+galliumMass = (188 - 3 * 16) / 2
+
+# Stoichiometric index
+indx = (182 - 2 * 35) / 16
+
+# Compound masses
+masses = [2 + 16 40 + 16 14 * 2]
+masses2 = [14 + 3 40 + 35 * 2 23 * 2 + 16]
+masses3 = [23 + 14 + 3 * 16 2 * 23 + 32 + 4 * 16]
+masses4 = [64 + 32 + 4 * 16 207 + 2 * 35 2 * 27 + 3 * (4 * 16 + 32)]
+masses5 = [2 * 27 + 3 * 16 32 + 3 * 16 24 + 2 * (16 + 1)]
+
+# Carbon (C)
+carbonMass = 106 - 3 * 16 - 2 * 23
+
+# Iron - Fe2O3
+ironMass = (160 - 3 * 16) / 2
+
+# Symbol C
+carbonMass = 106 - 3 * 16 - 2 * 23
+
+# Aluminum - Al(OH)3
+aluminumMass = 78 - 3 * (16 + 1)
 
 ########################################
